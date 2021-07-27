fbee5931f841191be48f33218a408c6b0134fa6c
Ruto <ruto@protonmail.ch>
2021-07-27 14:01:00 -0400

    Basic map page

---
+++ pages/maps/index.vue
@@ -0,0 +1,71 @@
+<template lang="pug">
+  div
+    h1 Maps
+    div(class="mx-14 py-4")
+    v-data-table(
+      :options.sync="options"
+      :loading="loading"
+      :headers="headers"
+      :items="maps"
+      :page.sync="page"
+      hide-default-footer
+      class="elevation-4"
+    )
+    pagination(
+      v-model="page"
+      @prev="prevPage"
+      @next="nextPage"
+      :max-page="pageMax"
+      :show-pages="true"
+      :show-max="true"
+    )
+</template>
+
+<script>
+import Pagination from '~/components/Pagination.vue';
+export default {
+  components: {
+    Pagination
+  },
+  data () {
+    return {
+      loading: true,
+      options: {},
+      headers: [
+        {
+          text: 'ID',
+          value: 'id'
+        },
+        { text: 'Name', value: 'name' },
+        { text: 'Filesize', value: 'filesize' },
+        { text: 'Difficulty', value: 'difficulty' },
+        { text: 'Validated', value: 'validated' }
+      ],
+      maps: [],
+      itemsPerPage: 10,
+      page: 1,
+      pageMax: 1
+    };
+  },
+  mounted () {
+    this.getMaps();
+  },
+  methods: {
+    async getMaps () {
+      this.loading = true;
+      const params = new URLSearchParams();
+      params.set('limit', 9999);
+      const result = await this.$axios.$get('https://kztimerglobal.com/api/v2.0/maps', { params });
+      this.maps = result;
+      this.pageMax = Math.ceil(this.maps.length / this.itemsPerPage);
+      this.loading = false;
+    },
+    prevPage () {
+      this.page--;
+    },
+    nextPage () {
+      this.page++;
+    }
+  }
+};
+</script>
