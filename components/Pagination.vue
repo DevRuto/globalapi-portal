b3340de056210f53b9d0e036bd993bdb8e7caf9c
Ruto <ruto@protonmail.ch>
2021-07-27 14:00:21 -0400

    Custom pagination component

---
+++ components/Pagination.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  div
+    v-btn(text icon @click="onPrev" :disabled="value === 1")
+      v-icon(v-text="'$prev'")
+    span(v-if="showPages") {{ value }}
+      span(v-if="showMax")  / {{ maxPage }}
+    v-btn(text icon @click="onNext" :disabled="maxPage !== -1 && maxPage == value")
+      v-icon(v-text="'$next'")
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      default: 1
+    },
+    maxPage: {
+      type: Number,
+      required: false,
+      default: -1
+    },
+    showPages: {
+      type: Boolean,
+      default: true
+    },
+    showMax: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    onPrev () {
+      if (this.value === 1) {
+        return;
+      }
+      this.$emit('prev');
+    },
+    onNext () {
+      if (this.value === this.maxPage) {
+        return;
+      }
+      this.$emit('next');
+    }
+  }
+};
+</script>
