0323e0c15801f7d62c60ba25482090a7229c669c
Ruto <ruto@protonmail.ch>
2021-07-28 18:03:20 -0400

    Server page

--- pages/servers/index.vue
+++ pages/servers/index.vue
@@ -1,7 +1,6 @@
 <template lang="pug">
   div
-    h1 Maps
-    div(class="mx-14 pb-4")
+    h1 Servers
     v-row(class="pb-4 mx-2")
       v-spacer
       pagination(
@@ -9,19 +8,28 @@
         @prev="prevPage"
         @next="nextPage"
         @first="page = 1"
+        @last="page = pageMax"
         :max-page="pageMax"
         :show-pages="true"
         :show-max="true"
         :show-first="true"
+        :show-last="true"
       )
     v-data-table(
       :loading="loading"
       :headers="headers"
-      :items="maps"
+      :items="servers"
       :page.sync="page"
       hide-default-footer
       class="elevation-5"
     )
+      template(v-slot:item.endpoint="{ item }")
+        span {{ item.ip }}:{{ item.port }}
+      template(v-slot:item.owner="{ item }")
+        v-btn(text target="_blank" color="green" :href="`http://steamcommunity.com/profiles/${item.owner_steamid64}`")
+          v-icon(left) mdi-steam
+          | {{ item.owner_steamid64 }}
+          v-icon(right) mdi-open-in-new
     v-row(class="pt-4 mx-2")
       v-spacer
       pagination(
@@ -29,10 +37,12 @@
         @prev="prevPage"
         @next="nextPage"
         @first="page = 1"
+        @last="page = pageMax"
         :max-page="pageMax"
         :show-pages="true"
         :show-max="true"
         :show-first="true"
+        :show-last="true"
       )
 </template>
 
@@ -46,36 +56,27 @@ export default {
   },
   data () {
     return {
-      loading: true,
+      loading: false,
       headers: [
         { text: 'ID', value: 'id' },
+        { text: 'Address', value: 'endpoint' },
         { text: 'Name', value: 'name' },
-        { text: 'Filesize', value: 'filesize' },
-        { text: 'Difficulty', value: 'difficulty' },
-        { text: 'Validated', value: 'validated' }
+        { text: 'Owner', value: 'owner' }
       ],
-      maps: [],
+      servers: [],
       itemsPerPage: 10,
       page: 1,
       pageMax: 1
     };
   },
-  head () {
-    return {
-      title: 'Maps'
-    };
-  },
   mounted () {
-    this.getMaps();
+    this.getServers();
   },
   methods: {
-    async getMaps () {
+    async getServers () {
       this.loading = true;
-      const params = new URLSearchParams();
-      params.set('limit', 9999);
-      const result = await this.$axios.$get(`${API_HOST}/api/v2.0/maps`, { params });
-      this.maps = result;
-      this.pageMax = Math.ceil(this.maps.length / this.itemsPerPage);
+      this.servers = await this.$axios.$get(`${API_HOST}/api/v2.0/servers?is_validated=true`);
+      this.pageMax = Math.ceil(this.servers.length / this.itemsPerPage);
       this.loading = false;
     },
     prevPage () {
